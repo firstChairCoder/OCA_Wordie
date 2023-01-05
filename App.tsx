f61d10fbcb7425b9a05950bdb60d8f77f6832aa6
firstChairCoder <md1qq7@gmail.com>
2023-01-05 16:38:52 +0100

    chore(font): add custom font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  Nunito_400Regular,
+  Nunito_600SemiBold,
+  Nunito_800ExtraBold,
+  useFonts
+} from "@expo-google-fonts/nunito";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Regular: Nunito_400Regular,
+    SemiBold: Nunito_600SemiBold,
+    ExtraBold: Nunito_800ExtraBold
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontSize: 32, fontFamily: "SemiBold" }}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +30,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    backgroundColor: "#fff",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: "center"
+  }
 });
